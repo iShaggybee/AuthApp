d540d931acbf1b610066aab9c57ee38ce6a72682
Vadim Kislov <kislov.v@gmail.com>
2022-04-08 01:40:22 +0300

    feat: refactoring storyboards & change keyboard settings

--- AuthApp/LoginViewController.swift
+++ AuthApp/LoginViewController.swift
@@ -13,14 +13,10 @@ class LoginViewController: UIViewController {
     
     private let userName = "User"
     private let password = "Password"
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         guard let welcomeVC = segue.destination as? WelcomeViewController else { return }
-        
+
         welcomeVC.userName = userName
     }
 
@@ -32,22 +28,42 @@ class LoginViewController: UIViewController {
         }
     }
     
-    @IBAction func onForgotUser() {
+    @IBAction func onRemindUserName() {
         showMessage(title: "Remind", message: "Your user name is User")
     }
     
-    @IBAction func onForgotPassword() {
+    @IBAction func onRemindPassword() {
         showMessage(title: "Remind", message: "Your password is Password")
     }
     
+    @IBAction func unwind(for segue: UIStoryboardSegue) {
+        userNameField.text = ""
+        passwordField.text = ""
+    }
     
     private func showMessage(title: String, message: String) {
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
         let alertAction = UIAlertAction(title: "Ok", style: .default)
         
         alert.addAction(alertAction)
-        
         present(alert, animated: true)
     }
 }
 
+extension LoginViewController: UITextFieldDelegate {
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == userNameField {
+            passwordField.becomeFirstResponder()
+        } else {
+            onLogin()
+            performSegue(withIdentifier: "transitionWelcomeVC", sender: nil)
+        }
+        
+        return true
+    }
+}
